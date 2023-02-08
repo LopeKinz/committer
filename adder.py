20a78197f7884aaf0779d466265c300939bb8be9
t.me/xtekky <98614666+xtekky@users.noreply.github.com>
2023-02-08 17:19:34 +0100

    _

---
+++ adder.py
@@ -0,0 +1,21 @@
+import random
+OUTPUT_FILE = "commit_bot.txt"
+
+from os import system
+from datetime import datetime 
+
+
+i = 0
+def create_commit():
+    global i
+    while True:
+        system("git pull -X theirs")
+        for i in range(200):
+            with open(OUTPUT_FILE, "w") as f:
+                f.write(str(datetime.now()))
+            system(f"git add {OUTPUT_FILE}")
+            system(f"git commit -m \"Update {OUTPUT_FILE}\"")
+        system("git pull -X theirs")
+        system("git push")
+
+create_commit()
